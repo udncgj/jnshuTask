de0d4f998dd6b297201226f13a1c4357611f9c46
udncgj <610445206@qq.com>
2017-02-19 21:58:01 +0800

    remove something

--- C++file/student/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	student.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
--- javafile/JDBCtest/src/MysqlTest/work.java
+++
@@ -1,5 +0,0 @@
-package MysqlTest;
-
-public class work {
-
-}
